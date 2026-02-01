9f2f11c9e7bb76a871e59704c81d0afce8f4f739
kushp <kushpraja6@gmail.com>
2026-01-31 21:39:23 -0500

    push

---
+++ kthSmallestInt.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        
+        queue = []
+
+        def dfs(root):
+            if not root:
+                return 
+            dfs(root.left)
+            queue.append(root.val)
+            dfs(root.right)
+
+        dfs(root)
+        return queue[k-1]
+
