b55465c7a5ba643f8dd736888606b4febaaab345
kushp <kushpraja6@gmail.com>
2026-02-09 10:25:31 -0500

    push

---
+++ kthLargestElement.py
@@ -0,0 +1,11 @@
+from typing import List
+import heapq
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        
+        nums = [-1 * n for n in nums]
+        heapq.heapify(nums)
+        for _ in range(k-1):
+            heapq.heappop(nums)
+        return nums[0] * -1
