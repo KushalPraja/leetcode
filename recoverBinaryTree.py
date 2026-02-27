da50fe01c93fbb20727f979ac046af39db03052f
kushp <kushpraja6@gmail.com>
2026-02-27 11:05:29 -0500

    push

---
+++ recoverBinaryTree.py
@@ -0,0 +1,31 @@
+
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        treelist = []
+        sets= []
+        def dfs(root):
+            dfs(root.left) if root.left else None
+            treelist.append(root)
+            sets.append(root.val)
+            dfs(root.right) if root.right else None
+
+        dfs(root)
+        sets = sorted(sets)
+
+        for i in range(len(treelist)):
+            if treelist[i].val != sets[i]:
+                treelist[i].val = sets[i]
+            
+
+
+            
+
+
+
