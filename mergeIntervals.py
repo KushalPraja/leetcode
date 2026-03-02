aa6a4f4242971ca3d5a65e9057b6fe6149ba306f
kushp <kushpraja6@gmail.com>
2026-03-02 15:59:43 -0500

    push

---
+++ mergeIntervals.py
@@ -0,0 +1,17 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        intervals.sort(key=lambda item:item[0])
+        stack = []
+
+        for i in range(len(intervals)):
+
+            if stack and intervals[i][0] <= stack[-1][1]:
+                stack[-1][0] = min(stack[-1][0], intervals[i][0])
+                stack[-1][1] = max(stack[-1][-1], intervals[i][-1])
+
+            else:
+                stack.append(intervals[i])
+
+        return stack
+        
