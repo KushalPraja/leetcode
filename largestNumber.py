cecdea9b838bc597cd90f6fb216e9038e1367b19
kushp <kushpraja6@gmail.com>
2026-03-05 10:20:32 -0500

    push

---
+++ largestNumber.py
@@ -0,0 +1,15 @@
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+    
+        for i in range(1, len(nums)):
+            idx = i
+            while idx > 0 and int(str(nums[idx]) + str(nums[idx-1])) >  int(str(nums[idx - 1]) + str(nums[idx])):
+                    nums[idx], nums[idx-1] = nums[idx-1], nums[idx]
+                    idx -= 1
+
+        x = "".join([str(i) for i in nums])
+        if int(x) == 0:
+            return "0"
+        else:
+            return x
