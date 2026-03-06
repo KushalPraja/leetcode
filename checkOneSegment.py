49885baa0995bc290c6ee112b398aefb0f526367
kushp <kushpraja6@gmail.com>
2026-03-06 11:44:55 -0500

    push

--- checkOneSegment.py
+++ checkOneSegment.py
@@ -1,12 +1,5 @@
 class Solution:
     def checkOnesSegment(self, s: str) -> bool:
-        
-        found = False
-        for i in range(len(s)):
-            if s[i] == "1" and found == False:
-                found = True
-            
-            elif found and i >= 1 and s[i] == "1" and s[i - 1] == "0":
-                return False
+        return "01" not in s
 
-        return True
+        
