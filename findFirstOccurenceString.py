a90f7fb3c056e9ad11953d435a110311d51b9a45
kushal <kushpraja6@gmail.com>
2026-04-07 20:08:27 -0400

    formatting

--- findFirstOccurenceString.py
+++ findFirstOccurenceString.py
@@ -3,6 +3,5 @@ class Solution:
         for l in range(len(haystack) - len(needle) + 1):
             if haystack[l:l + len(needle)] == needle:
                 return l
-
         return -1
            
