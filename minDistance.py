fdca669b09344596e066873baa3e74c0063e3565
kushp <kushpraja6@gmail.com>
2026-04-10 14:17:09 -0400

    Refactor minimumDistance function to improve efficiency by using a mapping for indices

--- minDistance.py
+++ minDistance.py
@@ -2,18 +2,18 @@ from typing import List
 
 class Solution:
     def minimumDistance(self, nums: List[int]) -> int:
-
-        distance = float('inf')
+        mapping = {}
 
         for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                for k in range(j + 1, len(nums)):
+            if nums[i] not in mapping:
+                mapping[nums[i]] = []
 
-                    if nums[i] == nums[j] == nums[k]:
-                        distance = min(distance, abs(i - j) + abs(j - k) + abs(k - i))
+            mapping[nums[i]].append(i)
 
-        
-        if distance == float('inf'):
-            return -1
-
-        return distance
+        distance = float('inf')
+        for i in list(mapping.values()):
+            if len(i) >= 3:
+                for j in range(len(i) - 2):
+                    distance = min(distance, abs(i[j] - i[j + 1]) + abs(i[j] - i[j + 2]) + abs(i[j + 1] - i[j + 2]))
+            
+        return distance if distance != float('inf') else -1         
