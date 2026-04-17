c38d68292973daab08fe9180635b19ef4f3ef8d1
kushp <kushpraja6@gmail.com>
2026-04-17 11:47:23 -0400

    mirror distance pair

---
+++ minMirrorDis.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def minMirrorPairDistance(self, nums: List[int]) -> int:
+        
+        mapping = {}
+        dis = float('inf')
+        
+        for i in range(len(nums)):
+            if nums[i] in mapping:
+                dis = min(dis, abs(i - mapping[nums[i]]))
+        
+            rev = int(str(nums[i])[::-1])
+            if rev not in mapping:
+                mapping[rev] = i
+        
+            else:
+                mapping[rev] = i
+        
+        
+        return dis if dis != float('inf') else -1
+            
