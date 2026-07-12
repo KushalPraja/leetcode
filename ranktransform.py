d54a3bef4c733fb82ab3ed0b1a69bc258406e567
kushal <kushpraja6@gmail.com>
2026-07-12 00:12:39 -0400

    push

---
+++ ranktransform.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        
+        temp = sorted(list(set(arr)))
+        hashmap = {}
+
+        for i in range(len(temp)):
+            hashmap[temp[i]] =  i + 1
+
+        for j in range(len(arr)):
+            arr[j] = hashmap[arr[j]]
+
+        
+        return arr
