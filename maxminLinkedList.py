443d36fbad1c469d37fcebdfd412ed014cca096f
kushal <kushpraja6@gmail.com>
2026-08-31 12:16:34 -0400

    Refactor ListNode class definition for clarity and organization

--- maxminLinkedList.py
+++ maxminLinkedList.py
@@ -1,8 +1,11 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+
+from typing import List, Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
