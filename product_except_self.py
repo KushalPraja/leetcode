05de2d02bfebaf8309b5e4d7c27f3070f6f8ed31
Kushal Praja <kushpraja6@gmail.com>
2026-09-16 11:49:15 -0400

    improved solution

--- product_except_self.py
+++ product_except_self.py
@@ -1,12 +1,26 @@
+from typing import List
+
 class Solution:
-    def productExceptSelf(self, nums: list[int]) -> list[int]:
-        return [self.multiply(idx, nums) for idx,val in enumerate(nums)]
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        prefix = [1] * len(nums)
+        suffix = [1] * len(nums)
 
-    def multiply(self, idx, listy):
-        product = 1;
-        for i in range(len(listy)):
-            if i != idx:
-                product *= listy[i]
-        return product
+        prefix[0] = nums[0]
+        suffix[-1] = nums[-1]
 
+        for i in range(1, len(nums)):
+            prefix[i] = prefix[i - 1] * nums[i]
+        
+        for i in range(len(nums) - 2, -1, -1):
+            suffix[i] = suffix[i + 1] * nums[i]
+        
+        temp = [0] * len(nums)
+        temp[0] = suffix[1]
+        temp[-1] = prefix[-2]
+
+        for i in range(1, len(temp) - 1):
+            temp[i] = prefix[i - 1] * suffix [i + 1]
+        
+        return temp
 
